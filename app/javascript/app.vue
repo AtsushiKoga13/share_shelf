349b59ddccc0ec5fdc8a7a20912b9aec0b9c0ebe
古賀敦 <shimauma444@gmail.com>
2020-10-08 14:48:39 +0900

    Add axios and user model and controlloer

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -2,15 +2,24 @@
   <div id="app">
     <p>{{ message }}</p>
     <p>{{ $store.state.message_2 }}</p>
+    <p>{{ user_info }}</p>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
+      message: "Hello Vue!",
+      user_info: []
     }
+  },
+  mounted () {
+    axios
+      .get('/users/index')
+      .then(response => (this.user_info = response.data))
   }
 }
 </script>
