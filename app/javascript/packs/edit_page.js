5ed282dbb7ae704f749b3ed901c2bf43a8b8eaed
古賀敦 <shimauma444@gmail.com>
2020-10-28 00:02:10 +0900

    Fix edit_page.js for image upload system

--- app/javascript/packs/edit_page.js
+++ app/javascript/packs/edit_page.js
@@ -1,3 +1,19 @@
 window.addEventListener('popstate', function(e) {
-  location.href="http://localhost:3000/edit";
+  location.href="/edit";
+});
+
+document.getElementById('user_image').addEventListener('change', function() {
+  console.log('Hello world!')
+  var uploadFile = document.getElementById("user_image").files[0];
+  var fileSize = uploadFile.size/1024/1024
+  if (fileSize > 5) {
+    alert("Maximum file size is 5MB. Please choose a smaller file.");
+  }
+  document.getElementById("result").innerHTML = fileSize;
+
+  var fileReader = new FileReader();
+  fileReader.onload = (function() {
+    document.getElementById('preview').src = fileReader.result;
+  });
+  fileReader.readAsDataURL(uploadFile);
 });
