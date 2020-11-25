a90e30d3d9a137108797b7aef58f8d36c0c7f827
古賀敦 <shimauma444@gmail.com>
2020-11-25 13:18:06 +0900

    Fix posts page design

--- app/javascript/components/Home.vue
+++ app/javascript/components/Home.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
-    <p>Posts</p>
+    <p class="font-weight-black display-1">Posts</p>
     <spinner v-show="spiner_loading"></spinner>
     <div v-show="!spiner_loading">
-      <ul id="example-1">
-        <li v-for="post in posts" :key="post.id">
-          <p>{{post.content}}</p>
-          <img :src="post_image(post)" :alt="post.image">
-        </li>
-      </ul>
+      <v-row>
+        <v-col class="pb-4" cols="12" xs="10" sm="9" md="7" v-for="post in posts" :key="post.id">
+          <v-card elevation="2">
+            <p class="mb-0 pt-1 pl-1 subtitle-1">{{post.content}}</p>
+            <v-row class="align-end">
+              <v-col xs="12" sm="6" md="6">
+                <img class="pb-3 pl-6" :src="post_image(post)" :alt="post.image">
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+      </v-row>
     </div>
   </div>
 </template>
